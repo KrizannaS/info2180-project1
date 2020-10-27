cbfd2d2b9f8c99b14d2c472a96c3f8cf12efe557
Krizanna Squire <krizanna28@gmail.com>
2020-10-26 21:10:47 -0500

    images add

--- scripts.js
+++ scripts.js
@@ -1,1 +1,18 @@
 /* Add your JavaScript to this file */
+function validate(){
+
+var validation=/\S+@\S+\.\S+/;
+var  inputx= validation.test(document.getElementById("email").value);
+var  val= document.getElementById("email").value;
+
+
+if(!inputx||inputx==null){
+alert("Please enter a valid email address.");
+}
+
+else{
+
+alert("Thank you! Your email address" +" " + val +"  has been to our mailing list!");
+}
+return inputx;
+}
